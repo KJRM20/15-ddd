b86a6eb3c484dda2c12104b0da11e8c7d1b5b6ba
Karen Rincón <karenrinconjm2016@gmail.com>
2025-02-27 14:36:50 -0500

    refactor: simplify sequential execution in round

--- src/scoreboards/src/application/src/main/java/com/uno/scoreboards/application/finishgameround/FinishGameRoundUseCase.java
+++ src/scoreboards/src/application/src/main/java/com/uno/scoreboards/application/finishgameround/FinishGameRoundUseCase.java
@@ -5,8 +5,10 @@ import com.uno.scoreboards.application.shared.round.RoundResponse;
 import com.uno.scoreboards.domain.round.Round;
 import com.uno.shared.application.ICommandUseCase;
 import com.uno.shared.domain.generic.DomainEvent;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Comparator;
 
 import static com.uno.scoreboards.application.shared.round.RoundMapper.mapToRound;
@@ -22,16 +24,20 @@ public class FinishGameRoundUseCase implements ICommandUseCase<FinishGameRoundRe
   public Mono<RoundResponse> execute(FinishGameRoundRequest request) {
     return repository.findEventsByAggregateId(request.getAggregateId())
       .collectList()
-      .map(events -> {
+      .flatMap(events -> {
         events.sort(Comparator.comparing(DomainEvent::getWhen));
         Round round = Round.from(request.getAggregateId(), events);
-        round.assignRoundWinner(request.getWinnerId(), request.getExtraPoints());
-        round.finishRound();
-        round.calculateResult();
-        round.getUncommittedEvents().forEach(repository::save);
-        round.markEventsAsCommitted();
 
-        return mapToRound(round);
+        return Flux.concat(
+            Mono.delay(Duration.ofMillis(100)).doOnNext(ignored -> round.assignRoundWinner(request.getWinnerId(), request.getExtraPoints())),
+            Mono.delay(Duration.ofMillis(100)).doOnNext(ignored -> round.finishRound()),
+            Mono.delay(Duration.ofMillis(100)).doOnNext(ignored -> round.calculateResult())
+          )
+          .then(Mono.fromCallable(() -> {
+            round.getUncommittedEvents().forEach(repository::save);
+            round.markEventsAsCommitted();
+            return mapToRound(round);
+          }));
       });
   }
 }
